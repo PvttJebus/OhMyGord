a9667defea0c84307a7dcef3b52b6102e4a2f79b
haedrichowen <owen.haedrich@mohawkcollege.ca>
2025-03-28 16:06:40 -0400

    Fix player animations and rolling. Simplify player into a single game object

--- Assets/Scripts/Physics Objects/Player.cs
+++ Assets/Scripts/Physics Objects/Player.cs
@@ -6,48 +6,36 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public Vector2 maxSpeed = new Vector2(7, 3);
-    public float jumpCooldownTime = 0.2f; // Cooldown duration after landing
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    Rigidbody2D body;
+    Ray2D ray;
+
+    Vector2 maxSpeed = new Vector2(7, 3);
+    float jumpCooldownTime = 0.2f; // Cooldown duration after landing
     float speedRatio = 1.5f;
     float stopSpeedRatio = 0.1f;
     float decay = 0.97f;
-    public float jumpSpeed = 1.3f;
-    Rigidbody2D body;
-    Ray2D ray;
-    Gamepad gamepad;
+    float jumpForce = 90f;
 
-    private bool wasGrounded = false;
-    private float jumpCooldownTimer = 0f;
+    bool wasGrounded = false;
+    float jumpCooldownTimer = 0f;
 
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        gamepad = Gamepad.current;  
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
         ray = new Ray2D(transform.position, Vector2.down);
     }
 
     void FixedUpdate()
     {
-        body.velocity = new Vector2(body.velocity.x * decay, body.velocity.y * decay);
-   
+        body.velocity = new Vector2(body.velocity.x * decay, body.velocity.y);
 
         // Movement input handling
-        if (up() && body.velocity.y < maxSpeed.y)
-        {
-            body.AddForce(Vector2.up * maxSpeed.y * speedRatio);
-        }
-        if (down() && body.velocity.y > -maxSpeed.y * speedRatio)
-        {
-            body.AddForce(Vector2.down * maxSpeed.y * speedRatio);
-        }
-        if (left() && body.velocity.x > -maxSpeed.x * speedRatio)
-        {
-            body.AddForce(Vector2.left * maxSpeed.x * speedRatio);
-        }
-        if (right() && body.velocity.x < maxSpeed.x * speedRatio)
-        {
-            body.AddForce(Vector2.right * maxSpeed.x * speedRatio);
-        }
+        body.AddForce(Vector2.right * Input.GetAxis("Horizontal") * maxSpeed.x * speedRatio);
+        body.AddForce(Vector2.up * Input.GetAxis("Vertical") * maxSpeed.y * speedRatio);
 
         bool isGrounded = grounded();
 
@@ -66,94 +54,33 @@ public class Player : MonoBehaviour
         wasGrounded = isGrounded;
 
         // Jump only if grounded and cooldown has expired
-        if (jump() && isGrounded && jumpCooldownTimer <= 0)
+        if (Input.GetButton("Jump") && isGrounded && jumpCooldownTimer <= 0)
         {
-            body.velocity += Vector2.up * jumpSpeed;
+            Debug.Log("Jump");
+            body.AddForce(Vector2.up * jumpForce);
         }
+
+        if (body.velocity.x > 0.05f)
+        {
+            animator.Play("PlayerWalk");
+            spriteRenderer.flipX = false;
+        }
+        else if (body.velocity.x < -0.05f)
+        {
+            animator.Play("PlayerWalk");
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            animator.Play("PlayerIdle");
+        }
+
+        spriteRenderer.transform.rotation = Quaternion.identity;
+
     }
     bool grounded()
     {
         var hit = Physics2D.Raycast(body.position + Vector2.down * 0.61f, Vector2.down, 0.001f);
         return hit.collider != null;
-    }
-
-    bool up()
-    {
-        if (Input.GetKey(KeyCode.W))
-        {
-            return true;
-        }
-        if (gamepad != null)
-        {
-            if (gamepad.dpad.up.isPressed)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool down()
-    {
-        if (Input.GetKey(KeyCode.S))
-        {
-            return true;
-        }
-        if (gamepad != null)
-        {
-            if (gamepad.dpad.down.isPressed)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool left()
-    {
-        if (Input.GetKey(KeyCode.A))
-        {
-            return true;
-        }
-        if (gamepad != null)
-        {
-            if (gamepad.dpad.left.isPressed)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool right()
-    {
-        if (Input.GetKey(KeyCode.D))
-        {
-            return true;
-        }
-        if (gamepad != null)
-        {
-            if (gamepad.dpad.right.isPressed)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool jump()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
-            return true;
-        }
-        if (gamepad != null)
-        {
-            if (gamepad.buttonSouth.isPressed)
-            {
-                return true;
-            }
-        }
-        return false;
     }
 }
--- Assets/Scripts/TimeAllotted.cs
+++ Assets/Scripts/TimeAllotted.cs
@@ -61,7 +61,5 @@ public class TimeAllotted : MonoBehaviour
 
             finalTime.text = TimeAlloted.ToString("F2") + " Seconds";
         }
-
-        Debug.Log(TimeAlloted);
     }
 }
